1c96f08cb0a783e35d6d40abe7ef09ab2616d405
Jeshurun Nasser <jeshurunnasser@gmail.com>
2025-06-19 16:40:52 +0200

    Preparation du projet pour Namechip

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,18 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ["full_name", "email", "message"]
+        widgets = {
+            "full_name": forms.TextInput(attrs={
+                'class': "mt-1 block w-full rounded-md bg-white border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 p-2",
+                'placeholder': "Enter Your full Name..."
+            }),
+            "email": forms.EmailInput(attrs={
+                'class': "mt-1 block w-full rounded-md bg-white border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 p-2",
+                'placeholder': "Enter Your Email..."
+            }),
+            "message": forms.Textarea(attrs={
+                'class': "mt-1 block w-full rounded-md bg-white border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 p-2",
+                'placeholder': "Enter Your Message...",
+                'rows': 4
+            }),
+        }
